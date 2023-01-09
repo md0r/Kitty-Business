3ce4dd5039fa22229b6850ef2509ed1fae489ba3
md0r <mihaidorhan@gmail.com>
2023-01-09 17:38:12 +0200

    Initial Commit

---
+++ RateMyCat/RateMyCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  RateMyCatApp.swift
+//  RateMyCat
+//
+//  Created by Mihai Dorhan on 09.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct RateMyCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
