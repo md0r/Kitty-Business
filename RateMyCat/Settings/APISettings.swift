0444db06d196a2c36bb8963187a0d18611407912
md0r <mihaidorhan@gmail.com>
2023-03-21 12:04:00 +0200

    update

--- RateMyCat/Settings/APISettings.swift
+++ RateMyCat/Settings/APISettings.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct APISettings {
     
-    static let api_keyParameter = "?api_key=live_DRPJhL9Y7S5xh9SdnvMevuNGZGXghTqM1MCFgY1otDfwi6qJlwdvJpzKed49s0zs"
+    static let api_keyParameter = "?api_key=API_KEY_HERE"
     
     static let getAllBreedsUrl = "https://api.thecatapi.com/v1/breeds"
     
